b418a776dd0b8609a564ab58eb89e2b6d7c768df
MrBiggie <dsy502yc@gmail.com>
2017-09-15 13:21:16 +1000

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+         int [] right = new int[124];
+         char [] pattern = {'a','v','s'};
+         System.out.println(pattern[1]);
+         System.out.println(right[pattern[2]]);
+
+         Stack<String> Try = new Stack();
+         Try.push("1");
+         Try.push("2");
+         Try.add("a");
+//         Try.remove(0);
+         System.out.println(Try.size());
+         Try.remove(0);
+
+        System.out.println(Try.indexOf("1"));
+         System.out.println(Try.get(1));
+         System.out.println(Try.peek());
+         System.out.println(Try.size());
+    }
+}
